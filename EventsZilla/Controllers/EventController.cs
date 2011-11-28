f9205d72a5fa599bb0996a79501784682c873274
Itamar Syn-Hershko <itamar@code972.com>
2011-11-28 19:02:44 +0200

    Adding EventController

---
+++ EventsZilla/Controllers/EventController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EventsZilla.Models;
+using HibernatingRhinos.Loci.Common.Controllers;
+
+namespace EventsZilla.Controllers
+{
+    public class EventController : RavenController
+    {
+        public ActionResult Index(int id, string slug)
+        {
+        	var e = RavenSession.Load<Event>(id);
+			if (e == null)
+				return HttpNotFound();
+
+			// Make sure the correct slug is being used in the URL
+			if (string.IsNullOrEmpty(slug) || !slug.Equals(e.Slug))
+				return RedirectToActionPermanent("Index", new {id = id, slug = e.Slug});
+
+            return View(e);
+        }
+    }
+}
---
+++ EventsZilla/Core/ContentHelpers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HibernatingRhinos.Loci.Common.Models;
+using MarkdownDeep;
+
+namespace EventsZilla.Core
+{
+	public static class ContentHelpers
+	{
+		public static MvcHtmlString CompiledMarkdownContent(this string str, bool trustContent = false)
+		{
+			if (str == null) return MvcHtmlString.Empty;
+
+			var md = new Markdown
+			         	{
+			         		AutoHeadingIDs = true,
+			         		ExtraMode = true,
+			         		NoFollowLinks = !trustContent,
+			         		SafeMode = false,
+			         		NewWindowForExternalLinks = true,
+			         	};
+
+			var contents = md.Transform(str);
+			return MvcHtmlString.Create(contents);
+		}
+	}
+}
